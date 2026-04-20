ec23504cccfc7332e70e4273f6dbbec70b4ca79b
Arseniy <catdog09073@gmail.com>
2026-04-20 22:52:45 +0300

    update README.md

--- src/modules/ColorMode/ColorMode.slice.ts
+++ src/modules/ColorMode/ColorMode.slice.ts
@@ -1,14 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const getTheme = () => {
-  const saved = localStorage.getItem("isDark");
-  return saved ? JSON.parse(saved) : window.matchMedia("(prefers-color-scheme: dark)").matches;
-}
-
 export const ColorModeSlice = createSlice({
   name: "colorMode",
   initialState: {
-    isDark: getTheme(),
+    isDark: false,
   },
   selectors: {
     isDark: (state) => state.isDark,
